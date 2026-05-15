4c7037bed37656bb8b3445756e22d3663625daa1
Bruno Ramos <79422907+rbrunob@users.noreply.github.com>
2026-05-15 10:41:13 -0300

    hello, world!

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-zinc-900">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center sm:items-center">
+        <h1 className="text-6xl font-bold text-center sm:text-left">
+          Welcome to BinaryInc!
+        </h1>
+        <span className="text-center font-medium text-zinc-500 text-xs">
+          Hello, world! - comming soon...
+        </span>
+      </main>
+    </div>
+  );
+}
